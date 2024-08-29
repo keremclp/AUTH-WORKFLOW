1d3064f8958ad078aad4a14fd4c20374b7bf913a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-29 18:44:33 +0300

    verify-email controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,6 +27,25 @@ const register = async (req, res) => {
   });
   
 };
+
+const verifyEmail = async (req,res) => {
+  const { verificationToken,email } = req.body;
+  const user = await User.findOne({ email });
+  if(!user){
+    throw new CustomError.UnauthenticatedError('Verification failed')
+  }
+  
+  if(user.verificationToken !== verificationToken){
+    throw new CustomError.UnauthenticatedError("Verification failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now()
+  user.verificationToken = '';
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg:'Email verified' })
+}
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -63,4 +82,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail
 };
